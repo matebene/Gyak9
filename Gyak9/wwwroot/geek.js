030b3212fc05189c5deb75d0a8dbd2087096f497
Máté Bene <bene,mate2002@gmail.com>
2022-11-23 13:02:42 +0100

    geek

---
+++ Gyak9/wwwroot/geek.js
@@ -0,0 +1,20 @@
+﻿var viccek;
+function letöltés() {
+    fetch('/jokes.json')
+        .then(response => response.json())
+        .then(data =>letöltésBefejeződött(data))
+};
+function letöltésBefejeződött(d) {
+    console.log("Sikeres letöltés");
+    console.log(d);
+    viccek = d;
+    let ide = document.getElementById("ide");
+    for (var i = 0; i < viccek.length; i++) {
+        let elem = document.createElement("li");
+        elem.innerHTML = viccek[i].text;
+        ide.appendChild(elem);
+    }
+}
+window.onload = function () {
+    letöltés();
+};
