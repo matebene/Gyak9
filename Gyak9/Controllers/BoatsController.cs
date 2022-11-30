c9e8eb704143d5292c408fff5f385c982fb8b012
Máté Bene <bene,mate2002@gmail.com>
2022-11-30 12:19:42 +0100

    hajo

---
+++ Gyak9/Controllers/BoatsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gyak9.Controllers
+{
+    public class BoatsController : Controller
+    {
+        [HttpGet]
+        [Route("questions/all")]
+        public IActionResult M1()
+        {
+            Models.HajosContext context = new Models.HajosContext();
+            var kérdések = from x in context.Questions select x.Question1;
+            return new JsonResult(kérdések);
+        }
+    }
+}
---
+++ Gyak9/Controllers/TesztController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gyak9.Controllers
+{
+    public class TesztController : Controller
+    {
+        [HttpGet]
+        [Route("corvinus/szerverido")]
+        public IActionResult M1()
+        {
+            string pontosIdő = DateTime.Now.ToShortTimeString();
+
+            return Ok(pontosIdő);
+        }
+        [HttpGet]
+        [Route("corvinus/nagybetus/{szoveg}")]
+        public IActionResult M2(string szoveg)
+        {
+            try { return Ok(szoveg.ToUpper()); 
+            }
+          catch { return BadRequest("Nem jó a bemenő adat"); };
+            
+            
+        }
+    }
+}
