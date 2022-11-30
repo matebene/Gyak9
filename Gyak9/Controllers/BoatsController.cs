69c7aaad75641fbd618ae36d1f2376828cfe928a
Máté Bene <bene,mate2002@gmail.com>
2022-11-30 13:10:01 +0100

    Hajo - kérdések betöltése

--- Gyak9/Controllers/BoatsController.cs
+++ Gyak9/Controllers/BoatsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Gyak9.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Gyak9.Controllers
 {
@@ -8,9 +9,18 @@ namespace Gyak9.Controllers
         [Route("questions/all")]
         public IActionResult M1()
         {
-            Models.HajosContext context = new Models.HajosContext();
+            HajosContext context = new HajosContext();
             var kérdések = from x in context.Questions select x.Question1;
             return new JsonResult(kérdések);
+        }
+        [HttpGet]
+        [Route("questions/{sorszám}")]
+        public  IActionResult M2(int sorszám)
+        {
+            HajosContext context = new HajosContext();
+            var kérdés = (from x in context.Questions where x.QuestionId==sorszám select x).FirstOrDefault();
+            if (kérdés == null) return BadRequest("Nincs ilyen sorszám");
+            return new JsonResult(kérdés);
         }
     }
 }
---
+++ Gyak9/wwwroot/main.js
@@ -0,0 +1,66 @@
+﻿window.onload = function (e) {
+    console.log("Oldal betöltve...");
+    document.getElementById("előre_gomb").onclick = előre;
+    document.getElementById("vissza_gomb").onclick = vissza;
+    kérdésBetöltés(questionId)
+}
+var jóVálasz;
+var questionId = 4;
+
+function kérdésMegjelenítés(kérdés) {
+    if (!kérdés) return; //Ha undefined a kérdés objektum, nincs mit tenni
+    console.log(kérdés);
+    document.getElementById("kérdés_szöveg").innerText = kérdés.questionText
+    document.getElementById("válasz1").innerText = kérdés.answer1
+    document.getElementById("válasz2").innerText = kérdés.answer2
+    document.getElementById("válasz3").innerText = kérdés.answer3
+    jóVálasz = kérdés.correctAnswer;
+    if (kérdés.image) {
+        document.getElementById("kép").src = "https://szoft1.comeback.hu/hajo/" + kérdés.image;
+        document.getElementById("kép").classList.remove("rejtett")
+    }
+    else {
+        document.getElementById("kép").classList.add("rejtett")
+    }
+    //Jó és rossz kérdések jelölésének levétele
+    document.getElementById("válasz1").classList.remove("jó", "rossz");
+    document.getElementById("válasz2").classList.remove("jó", "rossz");
+    document.getElementById("válasz3").classList.remove("jó", "rossz");
+}
+function válaszfeldolgozás(válasz) {
+    if (!válasz.ok) {
+        console.error(`Hibás válasz: ${response.status}`)
+    }
+    else {
+        return válasz.json()
+    }
+}
+function kérdésBetöltés(id) {
+    fetch(`/questions/${id}`)
+        .then(response => {
+            if (!response.ok) {
+                console.error(`Hibás válasz: ${response.status}`)
+            }
+            else {
+                kérdésMegjelenítés(response.json())
+            }
+        })
+}
+function előre() {
+    questionId++;
+    kérdésBetöltés(questionId)
+}
+
+function vissza() {
+    questionId--;
+    kérdésBetöltés(questionId)
+}
+function választás(n) {
+    if (n != jóVálasz) {
+        document.getElementById(`válasz${n}`).classList.add("rossz");
+        document.getElementById(`válasz${jóVálasz}`).classList.add("jó");
+    }
+    else {
+        document.getElementById(`válasz${jóVálasz}`).classList.add("jó");
+    }
+}
